df8c4c4e6c8ef2c7cdf63c683b5e9719fe9fb7fa
Shreyansh Support <techshreyansh.talk@gmail.com>
2025-05-07 10:15:01 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -7,12 +7,6 @@ API_HASH: str = os.getenv("API_HASH", "")
 
 SESSION_STRING: str = os.getenv("SESSION_STRING", "")
 
-app_version: str = os.getenv ("userbot_version", "")
-
-device_model": str = os.getenv ("f"Moon-Userbot @ {gitrepo.head.commit.hexsha[:7]}", "")
-
-system_version: str = os.getenv ("platform.version() + " " + platform.machine()" , "")
-
 OWNER_ID: list[int] = [int(id.strip()) for id in os.getenv("OWNER_ID", "0").split(",")]
 
 LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", 0))
