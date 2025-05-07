3fe55520d6a91bb34303d1c5965ae54e4bc08984
Shreyansh Support <techshreyansh.talk@gmail.com>
2025-05-07 10:50:05 +0530

    Update bot.py

--- YMusic/core/bot.py
+++ YMusic/core/bot.py
@@ -1,15 +1,25 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-import config
-from ..logging import LOGGER
+import platform
+from utils import config
+from utils.misc import gitrepo, userbot_version
 
 api_id: int = config.API_ID
 api_hash: str = config.API_HASH
 session_string: str = config.SESSION_STRING
 
 YMusicBot = Client(
-    name="YMusic", api_id=api_id, api_hash=api_hash, session_string=session_string
+    name="YMusic",
+    api_id=api_id,
+    api_hash=api_hash,
+    session_string=session_string,
+    device_model=f"YMusic-Userbot @ {gitrepo.head.commit.hexsha[:7]}",
+    system_version=platform.version() + " " + platform.machine(),
+    app_version=userbot_version,
+    workdir="./",
+    sleep_threshold=10,
+    parse_mode="html"
 )
 
 YMusicUser = PyTgCalls(YMusicBot)
