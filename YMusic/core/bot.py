acc9bbd5ced4ed84d669e3f5dae86f0304956136
Shreyansh Support <techshreyansh.talk@gmail.com>
2025-05-07 10:15:46 +0530

    Update bot.py

--- YMusic/core/bot.py
+++ YMusic/core/bot.py
@@ -7,14 +7,9 @@ from ..logging import LOGGER
 api_id: int = config.API_ID
 api_hash: str = config.API_HASH
 session_string: str = config.SESSION_STRING
-app_version: str = config.userbot_version
-device_model: str = config.f"KittyBeats @ {gitrepo.head.commit.hexsha[:7]}"
-system_version: str = config.platform.version() + " " + platform.machine()
 
 YMusicBot = Client(
-    name="YMusic", api_id=api_id, api_hash=api_hash, session_string=session_string app_version:userbot_version
-device_model:f"KittyBeats @ {gitrepo.head.commit.hexsha[:7]}"
-system_version:platform.version() + " " + platform.machine()
+    name="YMusic", api_id=api_id, api_hash=api_hash, session_string=session_string
 )
 
 YMusicUser = PyTgCalls(YMusicBot)
